d9d39a4394408d4759f3fef7beb3a416ff544f63
Philip Teow <ptxy.1897@gmail.com>
2018-03-17 15:33:26 +0800

    done custom program vc mostly

--- PhysioApp/Custom Program/CustomProgramViewController.swift
+++ PhysioApp/Custom Program/CustomProgramViewController.swift
@@ -22,23 +22,32 @@ class CustomProgramViewController: UIViewController {
     @IBAction func addButtonTapped(_ sender: UIBarButtonItem) {
         
         
-        ref.child("users").child(currentUserID).child("programs").childByAutoId()
         
         let path = ref.child("users").child(currentUserID).child("programs").childByAutoId()
         
-        let key = path.key
+        ref.child("users").child(currentUserID).child("programs").child(path.key).updateChildValues(["name" : "Program Name", "totalTime" : 0])
+        
         
         guard let vc = storyboard?.instantiateViewController(withIdentifier: "AddProgramViewController") as? AddProgramViewController else {return}
         
-        ref.child("users").child(currentUserID).child("programs").child(key).observe(.value) { (snapshot) in
-            guard let dict = snapshot.value as? [String : Any] else {return}
-            let aProgram = Program(programID: snapshot.key, dict: dict)
-            DispatchQueue.main.async {
-                vc.selectedProgram = aProgram
-                    
-                    self.navigationController?.pushViewController(vc, animated: true)
-            }
-        }
+        let dict : [String : Any] = ["name" : "Program Name"]
+        let aProgram = Program(programID: path.key, dict: dict)
+        
+        vc.selectedProgram = aProgram
+        
+        navigationController?.pushViewController(vc, animated: true)
+        
+        
+        
+//        ref.child("users").child(currentUserID).child("programs").child(key).observe(.value) { (snapshot) in
+//            guard let dict = snapshot.value as? [String : Any] else {return}
+//            let aProgram = Program(programID: snapshot.key, dict: dict)
+//            DispatchQueue.main.async {
+//                vc.selectedProgram = aProgram
+//
+//                    self.navigationController?.pushViewController(vc, animated: true)
+//            }
+//        }
         
         
 //        ref
@@ -52,6 +61,11 @@ class CustomProgramViewController: UIViewController {
         super.viewDidLoad()
         ref = Database.database().reference()
         currentUserID = Auth.auth().currentUser?.uid ?? ""
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        programs = []
+        loadPrograms()
     }
     
     
@@ -80,9 +94,9 @@ extension CustomProgramViewController : UICollectionViewDataSource {
         guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as? CustomProgramCollectionViewCell else {return UICollectionViewCell()}
         
 //        let photoURL = programs[indexPath.row].photoURL
-        
+        getImage("https://static1.squarespace.com/static/55a91762e4b0b8b2c8ec1996/55b270bce4b09435f235ba17/55b2715be4b09f505f14e9d0/1437757789668/Photo+2014-11-09%2C+12+38+07+PM.jpg", cell.imageView)
         cell.nameLabel.text = programs[indexPath.row].name
-        cell.timeLabel.text = String(programs[indexPath.row].totalTime)
+        cell.timeLabel.text = "Estimated Time : \(String(programs[indexPath.row].totalTime)) minutes"
         
         return cell
     }
