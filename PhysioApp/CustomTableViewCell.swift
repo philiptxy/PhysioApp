501952d458af9a22abddaa4d80d6d9bc191ebf52
cybotchua <cybotchua@hotmail.com>
2018-03-20 11:29:34 +0800

    added custom table view cell

---
+++ PhysioApp/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  PhysioApp
+//
+//  Created by Terence Chua on 19/03/2018.
+//  Copyright © 2018 Philip Teow. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+//    func setFrame(frame: CGRect) {
+//        var frame = frame
+//        frame.origin.x += inset
+//
+//    }
+
+}
